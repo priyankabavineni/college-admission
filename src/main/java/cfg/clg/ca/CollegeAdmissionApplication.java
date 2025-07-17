43ac16de78ff0d67b4bc081d7de835def684d3d9
priyankabavineni <priyankabavineni@gmail.com>
2025-07-17 12:01:32 +0530

    initial project setup

---
+++ src/main/java/cfg/clg/ca/CollegeAdmissionApplication.java
@@ -0,0 +1,13 @@
+package cfg.clg.ca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeAdmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeAdmissionApplication.class, args);
+	}
+
+}
