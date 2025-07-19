e43f81681bdd134be8eef22b09d430e2e174332c
nihithanarreddy <nihithanarreddy@gmail.com>
2025-07-19 15:07:01 +0530

    Add/update AdminService implementation

--- src/main/java/cfg/clg/service/AdminService.java
+++ src/main/java/cfg/clg/service/AdminService.java
@@ -1,3 +1,4 @@
+
 package cfg.clg.service;
 
 import java.util.List;
@@ -7,31 +8,36 @@ import org.springframework.stereotype.Service;
 
 import cfg.clg.entity.AdminEntity;
 import cfg.clg.entity.ApplicationEntity;
+import cfg.clg.exception.AdminNotFoundException;
+import cfg.clg.exception.ApplicationNotFoundException;
 import cfg.clg.repository.AdminRepository;
 import cfg.clg.repository.ApplicationRepository;
 
 @Service
 public class AdminService {
-	@Autowired
-	private ApplicationRepository applicationRepo;
-	
-	@Autowired
-	private AdminRepository adminRepo;
-	 public ApplicationEntity updateStatus(String applicationId, int adminId, String status) {
-	        ApplicationEntity application = applicationRepo.findById(applicationId)
-	            .orElseThrow(() -> new RuntimeException("Application not found"));
-	        AdminEntity admin = adminRepo.findById(adminId)
-	            .orElseThrow(() -> new RuntimeException("Admin not found"));
 
-	        application.setStatus(status.toUpperCase());
-	        application.setAdmin(admin);
+    @Autowired
+    private ApplicationRepository applicationRepo;
 
-	        return applicationRepo.save(application);
-	    }
+    @Autowired
+    private AdminRepository adminRepo;
 
-	    // ADMIN: View All Applications
-	    public List<ApplicationEntity> getAllApplications() {
-	        return applicationRepo.findAll();
-	    }
-	}
+    public ApplicationEntity updateStatus(String applicationId, int adminId, String status) {
+        ApplicationEntity application = applicationRepo.findById(applicationId)
+                .orElseThrow(() -> new ApplicationNotFoundException("Application id not found: "+applicationId ));
+
+        AdminEntity admin = adminRepo.findById(adminId)
+                .orElseThrow(() -> new AdminNotFoundException("Admin id not found: "+adminId));
+
+        application.setStatus(status.toUpperCase());
+        application.setAdmin(admin);
+
+        return applicationRepo.save(application);
+    }
+
+    public List<ApplicationEntity> getAllApplications() {
+        return applicationRepo.findAll();
+    }
+}
+
 
