92f3445215933589dabb42d56cf218026e604ebc
priyankabavineni <priyankabavineni@gmail.com>
2025-07-19 15:14:37 +0530

    Updated ApplicationController.java with latest changes

--- src/main/java/cfg/clg/controller/ApplicationController.java
+++ src/main/java/cfg/clg/controller/ApplicationController.java
@@ -1,20 +1,57 @@
 package cfg.clg.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import cfg.clg.dto.ApplicationDto;
+import cfg.clg.entity.ApplicationEntity;
+import cfg.clg.exception.AdminNotFoundException;
+import cfg.clg.exception.ProgramNotFoundException;
+import cfg.clg.exception.StudentNotFoundException;
 import cfg.clg.service.ApplicationService;
+import cfg.clg.controller.ResponseData;
 
 @RestController
+@RequestMapping("/api")
 public class ApplicationController {
 
     @Autowired
     private ApplicationService applicationService;
 
     @PostMapping("/applicationsave")
-    public ApplicationDto saveApplication(@RequestBody ApplicationDto dto) {
-        return applicationService.processApplication(dto);
+    public ResponseData saveApplication(@RequestBody ApplicationDto dto) {
+        ResponseData response = new ResponseData();
+
+        try {
+            ApplicationDto savedDto = applicationService.processApplication(dto);
+            response.setStatus("success");
+            response.setData(savedDto);
+        } catch (ProgramNotFoundException | StudentNotFoundException | AdminNotFoundException ex) {
+            response.setStatus("failed");
+            response.setMessage(ex.getMessage());
+        } catch (Exception ex) {
+            response.setStatus("failed");
+            response.setMessage("Internal error: " + ex.getMessage());
+        }
+
+        return response;
     }
 
+    @GetMapping("/allApplications")
+    public ResponseData getAllApplications() {
+        ResponseData response = new ResponseData();
+
+        try {
+            List<ApplicationEntity> applications = (List<ApplicationEntity>) applicationService.getAllApplications();
+            response.setStatus("success");
+            response.setData(applications);
+        } catch (Exception ex) {
+            response.setStatus("failed");
+            response.setMessage("Error fetching applications: " + ex.getMessage());
+        }
+
+        return response;
+    }
 }
